894ca254722b8d38ea2e9484039b2eecdcf29063
psmbharti <ps.mbharti@outlook.com>
2026-05-09 16:30:23 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import psycopg2
 SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
 def get_db():
     return psycopg2.connect(
-        host=os.getenv("https://online-shop-6.onrender.com"),
+        host=os.getenv("dpg-d7vottbrjlhs73duc5n0-a"),
         database=os.getenv("online_shop_db_ixcd"),
         user=os.getenv("online_shop_db_ixcd_user"),
         password=os.getenv("W8n91glskNtNwqkSD8YDGxcMF1rNBIN7"),
